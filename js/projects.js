064e00211a1be6bc7e7840cacebb96f000fddd73
mgozon <mgozon@umich.edu>
2021-10-29 23:45:11 -0400

    added descriptions for multiple projects

--- js/projects.js
+++ js/projects.js
@@ -20,7 +20,12 @@ let data = [
         title: "Musical Math",
         tags: ['Javascript', 'React', 'Tailwind CSS'],
         summary: 'Representing mathematics through music',
-        info: [`description`],
+        info: [`Musical Math is a web app that allows a user to input mathematical expressions to output audio and its corresponding sheet music.
+                The user could then select certain values from these functions and play them at configurable times in order to create a more cohesize piece.
+                Three example demos have been provided.`,
+                `This project came from MHacks, a hackathon hosted by the University of Michigan.
+                We pondered the question, what would math sound like, with all of its patterns and symmetries, if we converted it to music?
+                Certain functions, like trig functions, are cyclical in nature and could potentially create a musical pattern, while expressions involving modular arithmetic with powers of integers reflect the symmetries and patterns found in number theory.`],
         links: [
             ['Github', 'https://github.com/Dophin2009/mm'],
             ['Website', "https://dophin2009.github.io/mm/"]
@@ -31,8 +36,12 @@ let data = [
     {
         title: "Tasks with Coins",
         tags: ['Javascript', 'React', 'Tailwind CSS', 'Firebase'],
-        summary: 'quick summary',
-        info: [`description`],
+        summary: 'An incentivized to-do list',
+        info: [`Tasks with Coins aims to solve the problem: how can we make ourselves motivated to complete tasks that we just want to ignore?
+                Unlike other to-do list apps, Tasks with Coins includes a dual coin system flexible to the user's needs:
+                a single coin could not possibly capture the value of various tasks, while a plethora would overwhelm the user, making two an ideal amount.`,
+                `Tasks to be completed take on a button form to maximize efficiency while maintaining a simplistic design.
+                Once the user has gathered enough coins, they can create wishes and specify the amount they have to earn before they can fulfill their wish.`],
         links: [
             ['Website', "https://tasks-with-coins.web.app/"]
         ],
@@ -42,8 +51,14 @@ let data = [
     {
         title: "Color Board",
         tags: ['Unity', 'C#', 'Data Structures'],
-        summary: 'quick summary',
-        info: [`description`],
+        summary: 'A family-friendly board game to play over the holidays',
+        info: [`While playing monopoly over Christmas, I thought about the many board games with simple rules yet are difficult to predict who would win.
+                In this family-friendly game, one must strategically place their chips in such a way that they maximize the colored edges and 'edge streaks' that they own.
+                Currently supporting 2-8 players, Color Board contains 5 short Christmas bops to celebrate the Jolly Holidays.`,
+                `Thinking back to my experience with math competitions, there would oftentimes be problems involving two people playing optimally in an impartial game, where theory has been well developed (nim and the sprague-grundy theorem).
+                However, impartial games are harder to analyze, and with the addition of more than two people, a more difficult game could be formed, although strategies could still be developed.
+                Combining this idea with a board game naturally resulted in Color Board.
+                Using a disjoint set, the scoring can be efficiently calculated in log(n) time.`],
         links: [
             ['Website', "https://mgozon.github.io/Color-Board-Web/"]
         ],
@@ -53,8 +68,10 @@ let data = [
     {
         title: "Maze Generator",
         tags: ['Unity', 'C#', 'Algorithms'],
-        summary: 'quick summary',
-        info: [`description`],
+        summary: 'A game that generates its own mazes',
+        info: [`The idea of generating mazes had been once since I was a child, but only until I stepped into the competition scene did I realize how much more was possible with Data Structures and Algorithms.
+                There were many algorithms that I considered in the process of generating a maze, some that used a depth-first search, some that involved randomness to form a uniform spanning tree, while others that used a randomized minimum spanning tree algorithm.
+                As the first produced long corridors with the second taking up a considerable amount of time for large graphs, I took on the third approach, and a demo of how the maze is generated is available directly to the curious individual within the app.`],
         links: [
             ['App Store', "https://play.google.com/store/apps/details?id=com.MCGBlueDragon.MazeGenerator"]
         ],
@@ -64,7 +81,7 @@ let data = [
     {
         title: "Infinite Maze",
         tags: ['Unity', 'C#', 'Data Structures'],
-        summary: 'quick summary',
+        summary: 'a non-euclidean maze that only goes deeper',
         info: [`description`],
         links: [
             ['Website', "https://mgozon.github.io/Infinite-Maze-Web/"]
